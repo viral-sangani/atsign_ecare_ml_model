bb1764ced9ba007ce0ce040f3f153bdcb8a0b32a
Viral Sangani <virals@geekyants.com>
2021-07-07 14:15:19 +0530

    heroku testing

--- apis/views.py
+++ apis/views.py
@@ -13,27 +13,30 @@ def getResponse(request):
                 {"error": "File not found"}, status=status.HTTP_400_BAD_REQUEST
             )
         file = request.data["file"]
+        print(file)
+        print("Hello World")
         img_bytes = file.read()
-        dict = get_prediction_MNIST(image_bytes=img_bytes)
-        item = {}
-        for i in dict:
-            item[i] = str(dict[i])
-        return Response(
-            {
-                "data": {
-                    "COVID19": item[0],
-                    "Lung Opacity": item[1],
-                    "Normal": item[2],
-                    "Viral Pneumonia": item[3],
-                    "Atelectasis": item[4],
-                    "Consolidation": item[5],
-                    "Infiltration": item[6],
-                    "Pneumothorax": item[7],
-                    "Edema": item[8],
-                    "Emphysema": item[9],
-                    "Fibrosis": item[10],
-                    "Effusion": item[11],
-                }
-            },
-            status=status.HTTP_200_OK,
-        )
+        return Response({"Hi": "111"})
+        # dict = get_prediction_MNIST(image_bytes=img_bytes)
+        # item = {}
+        # for i in dict:
+        #     item[i] = str(dict[i])
+        # return Response(
+        #     {
+        #         "data": {
+        #             "COVID19": item[0],
+        #             "Lung Opacity": item[1],
+        #             "Normal": item[2],
+        #             "Viral Pneumonia": item[3],
+        #             "Atelectasis": item[4],
+        #             "Consolidation": item[5],
+        #             "Infiltration": item[6],
+        #             "Pneumothorax": item[7],
+        #             "Edema": item[8],
+        #             "Emphysema": item[9],
+        #             "Fibrosis": item[10],
+        #             "Effusion": item[11],
+        #         }
+        #     },
+        #     status=status.HTTP_200_OK,
+        # )
